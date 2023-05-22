b3c0dded35d5629ca92248535e56fa13d2fc7ef8
bgdnvk <bnovykov@gmail.com>
2023-05-22 21:02:24 +0200

    k8s

---
+++ backend/kubernetes/start.sh
@@ -0,0 +1,4 @@
+kubectl apply -f deployment-microservice-a.yaml
+kubectl apply -f service-microservice-a.yaml
+kubectl apply -f deployment-microservice-b.yaml
+kubectl apply -f service-microservice-b.yaml
